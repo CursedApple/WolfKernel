d203f7d75d469d2a8cc603ab4dab11c64feeadb5
seba3567 <sebastian26481@gmail.com>
2019-07-17 18:37:15 -0400

    fix conflix

--- sound/soc/msm/qdsp6v2/q6core.c
+++ sound/soc/msm/qdsp6v2/q6core.c
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-/* Copyright (c) 2012-2019, The Linux Foundation. All rights reserved.
-=======
 /* Copyright (c) 2012-2017, 2019, The Linux Foundation. All rights reserved.
->>>>>>> 9133dc5a929666fe408d0e1ff976f39df221041f
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
